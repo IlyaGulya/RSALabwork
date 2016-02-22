f45494242d2d6bbee09c9fc78110c7d5666d07e2
Ilya Gulya <gulya.i@devicement.com>
2016-02-23 00:22:55 +0600

    Lab complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,38 @@
 `use strict`;
 
+var debug = false;
+
+if(Array.prototype.equals)
+    console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
+// attach the .equals method to Array's prototype to call it on any array
+Array.prototype.equals = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
+
+    // compare lengths - can save a lot of time
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].equals(array[i]))
+                return false;
+        } else if (this[i] instanceof Big && array[i] instanceof Big) {
+          if (!this[i].eq(array[i]))
+                return false;
+        } else if (this[i] != array[i]) {
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;
+        }
+    }
+    return true;
+}
+// Hide method from for-in loops
+Object.defineProperty(Array.prototype, "equals", {enumerable: false});
+var lab;
 $(function() {
   $(window).on('hashchange',function() {
     $.sammy(function(){
@@ -36,7 +69,7 @@ $(function() {
   }
   $.sammy.apply()
 
-  var lab = new Labwork();
+  lab = new Labwork();
 
   $("button[data-step]").on('click', function() {
     var stepName = $(this).data("step");
@@ -66,12 +99,165 @@ var Labwork = function() {
     "inputMessageSequenceDecodedExample",
     "messageSequenceDecodedExampleHumanReadable"
   ];
-  createTable(this.codes);
-//  this.hideAllSteps()
+  this.transcode = createTable();
+  if (!debug) {
+    this.hideAllSteps()
+  } else {
+    this.fillExampleData()
+  }
   this.data = {};
   this.data.step = "inputPQ";
   this.showCurrentStep();
 }
+
+Labwork.prototype.fillExampleData = function () {
+  $("#numberP").val(7)
+  $("#numberQ").val(11)
+  $("#numberN").val(77)
+  $("#numberC").val(60)
+  $("#numberD").val(7)
+  $("#numberE").val(43)
+  $("#publickey-1").val(43)
+  $("#publickey-2").val(77)
+  $("#privatekey-1").val(7)
+  $("#privatekey-2").val(77)
+  var message = "Проверка 123"
+  var sequence = this.textToSequence(message)
+  var encoded = encode(43, 77, sequence)
+  $("#message-encode").val(message)
+  $("#message-sequence").val(sequence.join(","))
+  $("#message-sequence-encoded").val(encoded.join(","))
+  $("#numberWrongD").val(11)
+  var decodedWrong = decode(11, 77, encoded)
+  $("#message-sequence-decoded-wrong").val(decodedWrong)
+  var decodedWrongMessage = this.sequenceToText(decodedWrong);
+  $("#message-decoded-wrong").val(decodedWrongMessage)
+}
+
+Labwork.prototype.inputMessage = function (button) {
+  var messageInput = $("#message-encode");
+  var message = messageInput.val().trim();
+  if (message != "") {
+    this.data.message = message
+    this.data.messageSequence = this.textToSequence(message)
+    this.data.messageSequenceEncoded = encode(this.data.e, this.data.n, this.data.messageSequence)
+    disable(messageInput)
+    return true
+  }
+  return false
+}
+
+Labwork.prototype.inputMessageSequence = function (button) {
+  var sequenceInput = $("#message-sequence");
+  var sequence = sequenceInput.val().trim();
+  if (sequence != "") {
+    var messageSequence = this.data.messageSequence
+    var array = sequence.split(",")
+    if (!array.equals(messageSequence)) {
+      error(sequenceInput)
+    } else {
+      disable(sequenceInput)
+      return true
+    }
+  }
+  return false
+}
+
+Labwork.prototype.inputMessageSequenceEncoded = function (button) {
+  var sequenceInput = $("#message-sequence-encoded");
+  var sequence = sequenceInput.val().trim();
+  if (sequence != "") {
+    var messageSequenceEncoded = this.data.messageSequenceEncoded
+    var array = sequence.split(",")
+    if (!array.equals(messageSequenceEncoded)) {
+      error(sequenceInput)
+    } else {
+      disable(sequenceInput)
+      return true
+    }
+  }
+  return false
+}
+
+Labwork.prototype.inputWrongD = function (button) {
+  var wrongDInput = $("#numberWrongD");
+  var wrongD = getPrimeOrNotify(wrongDInput);
+  if (wrongD != -1) {
+    this.data.wrongD = wrongD
+    this.data.messageSequenceDecodedWrong = decode(this.data.wrongD, this.data.n, this.data.messageSequenceEncoded)
+    $("#message-decoded-wrong").val(this.sequenceToText(this.data.messageSequenceDecodedWrong))
+    disable(wrongDInput)
+    return true
+  }
+  return false
+}
+
+Labwork.prototype.inputMessageSequenceDecodedWrong = function (button) {
+  var sequenceInput = $("#message-sequence-decoded-wrong");
+  var sequence = sequenceInput.val().trim();
+  if (sequence != "") {
+    var messageSequenceDecodedWrong = this.data.messageSequenceDecodedWrong
+    var array = sequence.split(",")
+    for (var i in array) {
+      array[i] = parseInt(array[i])
+    }
+    if (!array.equals(messageSequenceDecodedWrong)) {
+      error(sequenceInput)
+    } else {
+      disable(sequenceInput)
+      this.data.messageExample = getRandomMessage();
+      this.data.messageExampleSequence = this.textToSequence(this.data.messageExample)
+      this.data.messageExampleSequenceEncoded = encode(this.data.e, this.data.n, this.data.messageExampleSequence)
+      $("#message-sequence-encoded-example").val(this.data.messageExampleSequenceEncoded.join(","))
+      if (debug) {
+        $("#message-sequence-decoded-example").val(this.data.messageExampleSequence.join(","))
+      }
+      return true
+    }
+  }
+  return false
+}
+
+
+Labwork.prototype.inputMessageSequenceDecodedExample = function (button) {
+  var sequenceInput = $("#message-sequence-decoded-example");
+  var sequence = sequenceInput.val().trim();
+  if (sequence != "") {
+    var messageExampleSequence = this.data.messageExampleSequence
+    var array = sequence.split(",")
+    for (var i in array) {
+      array[i] = parseInt(array[i])
+    }
+    if (!array.equals(messageExampleSequence)) {
+      error(sequenceInput)
+    } else {
+      disable(sequenceInput)
+      $("#message-decoded-example").val(this.data.messageExample)
+      return true
+    }
+  }
+  return false
+}
+
+encode = function (e, n, sequenceArray) {
+  var encodedArray = []
+  for (var i in sequenceArray) {
+    encodedArray[i] = encodeChar(e, n, sequenceArray[i])
+  }
+  return encodedArray
+}
+
+decode = function (d, n, sequenceArray) {
+  var decodedArray = []
+  for (var i in sequenceArray) {
+    decodedArray[i] = decodeChar(d, n, sequenceArray[i])
+  }
+  return decodedArray
+}
+
+var encodeChar = (e, n, char) => parseInt(new Big(char).pow(e).mod(n))
+
+var decodeChar = (d, n, char) => parseInt(new Big(char).pow(d).mod(n))
 
 Labwork.prototype.hideAllSteps = function () {
   $(this.steps).each((i,step) => this.hideStep(step));
@@ -234,6 +420,29 @@ Labwork.prototype.inputPrivateKey = function (button) {
   return false
 }
 
+Labwork.prototype.textToSequence = function (message) {
+  var result = [];
+  var messageArray = message.split("");
+  for (var i in messageArray) {
+    var encoded = this.transcode.code[messageArray[i]];
+    if (encoded != undefined) {
+      result.push(encoded)
+    }
+  }
+  return result;
+}
+
+Labwork.prototype.sequenceToText = function (sequenceArray) {
+  var result = [];
+  for (var i in sequenceArray) {
+    var encoded = this.transcode.symbol[sequenceArray[i]];
+    if (encoded != undefined) {
+      result.push(encoded)
+    }
+  }
+  return result.join("");
+}
+
 var disable = function(input) {
   $(input).prop('disabled', true)
 }
@@ -327,7 +536,7 @@ var getPrimeOrNotify = function (elem) {
 var createTable = function() {
   var codes = {
     number: "0123456789",
-    pointing: ".,!?;:",
+    pointing: ".,!?;: ",
     russianLower1: "абвгдеёжзийклмно",
     russianLower2: "прстуфхцчшщъыьэюя",
     russianUpper1: "АБВГДЕЁЖЗИЙКЛМНО",
@@ -337,8 +546,8 @@ var createTable = function() {
     englishUpper1: "ABCDEFGHIJKLM",
     englishUpper2: "NOPQRSTUVWXYZ"
   }
-  this.codeToSymbol = {};
-  this.symbolToCode = {};
+  var codeToSymbol = {};
+  var symbolToCode = {};
   var currentCode = 0;
   for (var key in codes) {
     var value = codes[key]
@@ -346,11 +555,26 @@ var createTable = function() {
     var trCode = $(`#${key}Code`);
     for (var i in value) {
       var symbol = value[i];
-      this.codeToSymbol[currentCode] = symbol;
-      this.symbolToCode[symbol] = currentCode;
+      codeToSymbol[currentCode] = symbol;
+      symbolToCode[symbol] = currentCode;
       $(`<td>${symbol}</td>`).appendTo(trSymbol);
       $(`<td>${currentCode}</td>`).appendTo(trCode);
       currentCode++;
     }
   }
+  return {
+    code: symbolToCode,
+    symbol: codeToSymbol
+  }
+}
+
+var messages = [
+  "Скоро госы?",
+  "Как вам СДЭС?",
+  "Автоматическая электросвязь",
+  "RSA это просто"
+]
+
+var getRandomMessage = function() {
+  return messages[Math.floor(Math.random() * messages.length)]
 }
